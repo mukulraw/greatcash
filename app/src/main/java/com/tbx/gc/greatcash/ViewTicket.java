beee783d631022dc1944e7e1e92e3bf1dca9810c
mukul <mukulraw199517@gmail.com>
2018-08-06 21:57:22 +0530

    done

--- app/src/main/java/com/tbx/gc/greatcash/ViewTicket.java
+++ app/src/main/java/com/tbx/gc/greatcash/ViewTicket.java
@@ -38,12 +38,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class ViewTicket extends Fragment
-{
+public class ViewTicket extends Fragment {
 
-    private TextView text_Name,text_ID;
+    private TextView text_Name, text_ID;
     SharedPreferences pref;
-    private Button btn_recentTicket,btn_submit;
+    private Button btn_recentTicket, btn_submit;
     private EditText ed_typed;
     private ProgressBar progressBar;
     private String str_typed;
@@ -52,8 +51,8 @@ public class ViewTicket extends Fragment
     private RecyclerView.LayoutManager layoutManager_question;
     private ProgressBar progressBar_question;
     private QuestionAdapter adapter;
-    private List<DataQuestion>dataQuestionList=new ArrayList<>();
-    String id,que_id;
+    private List<DataQuestion> dataQuestionList = new ArrayList<>();
+    String id, que_id;
     private TextView text_sel;
     private android.app.AlertDialog dialog;
 
@@ -62,74 +61,65 @@ public class ViewTicket extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.viewticket , container , false);
-        pref = getActivity().getSharedPreferences("pref" , Context.MODE_PRIVATE);
+        View view = inflater.inflate(R.layout.viewticket, container, false);
+        pref = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
 
-        text_Name=view.findViewById(R.id.text_userName);
-        text_ID=view.findViewById(R.id.text_ID);
-        btn_recentTicket=view.findViewById(R.id.signupgreatcash);
-        btn_submit=view.findViewById(R.id.btn_submit);
-        ed_typed=view.findViewById(R.id.ed_textTyped);
-        progressBar =view.findViewById(R.id.progress);
-        linearLayout_que=view.findViewById(R.id.linear_question);
-        text_sel=view.findViewById(R.id.text_sel_que);
+        text_Name = view.findViewById(R.id.text_userName);
+        text_ID = view.findViewById(R.id.text_ID);
+        btn_recentTicket = view.findViewById(R.id.signupgreatcash);
+        btn_submit = view.findViewById(R.id.btn_submit);
+        ed_typed = view.findViewById(R.id.ed_textTyped);
+        progressBar = view.findViewById(R.id.progress);
+        linearLayout_que = view.findViewById(R.id.linear_question);
+        text_sel = view.findViewById(R.id.text_sel_que);
 
-        String regID=pref.getString("refId", "");
-        Log.e("rrrriddddd",""+regID);
+        String regID = pref.getString("refId", "");
+        Log.e("rrrriddddd", "" + regID);
 
-        String userName=pref.getString("name", "");
-        Log.e("userNAme",""+userName);
+        String userName = pref.getString("name", "");
+        Log.e("userNAme", "" + userName);
 
-        id= pref.getString("id" , "");
+        id = pref.getString("id", "");
 
-        text_Name.setText("Name:"+userName);
-        text_ID.setText("Refer ID:"+regID);
+        text_Name.setText("Name:" + userName);
+        text_ID.setText("Refer ID:" + regID);
 
-        btn_submit.setOnClickListener(new View.OnClickListener()
-        {
+        btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                 str_typed=ed_typed.getText().toString();
+            public void onClick(View view) {
+                str_typed = ed_typed.getText().toString();
 
-                 String text_question=text_sel.getText().toString();
+                String text_question = text_sel.getText().toString();
 
-                if(str_typed.equals(""))
-                {
-                    Toast.makeText(getActivity(),"Fill the feild",Toast.LENGTH_SHORT).show();
+                if (str_typed.equals("")) {
+                    Toast.makeText(getActivity(), "Fill the feild", Toast.LENGTH_SHORT).show();
+                } else if (text_question.equals("")) {
+                    Toast.makeText(getActivity(), "Select the question", Toast.LENGTH_SHORT).show();
+                } else {
+                    HitApi();
                 }
-                else if(text_question.equals(""))
-                {
-                    Toast.makeText(getActivity(),"Select the question",Toast.LENGTH_SHORT).show();
-                }
-                else
-                    {
-                        HitApi();
-                    }
 
 
             }
         });
 
 
-        linearLayout_que.setOnClickListener(new View.OnClickListener()
-        {
+        linearLayout_que.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
 
-                View alertLayout = LayoutInflater.from(getActivity()).inflate(R.layout.question_dialog,null);
-                android.app.AlertDialog.Builder alertDialog=new android.app.AlertDialog.Builder(getActivity());
+                View alertLayout = LayoutInflater.from(getActivity()).inflate(R.layout.question_dialog, null);
+                android.app.AlertDialog.Builder alertDialog = new android.app.AlertDialog.Builder(getActivity());
                 alertDialog.setView(alertLayout);
-                 dialog=alertDialog.create();
+                dialog = alertDialog.create();
 
-                recyclerView_question=alertLayout.findViewById(R.id.recyclerView_question);
-                progressBar_question=alertLayout.findViewById(R.id.progressBar_question);
+                recyclerView_question = alertLayout.findViewById(R.id.recyclerView_question);
+                progressBar_question = alertLayout.findViewById(R.id.progressBar_question);
 
-                layoutManager_question=new LinearLayoutManager(getActivity());
+                layoutManager_question = new LinearLayoutManager(getActivity());
                 recyclerView_question.setLayoutManager(layoutManager_question);
 
-                adapter=new QuestionAdapter(getActivity(),dataQuestionList);
+                adapter = new QuestionAdapter(getActivity(), dataQuestionList);
                 recyclerView_question.setAdapter(adapter);
 
                 question_Api();
@@ -143,8 +133,7 @@ public class ViewTicket extends Fragment
         return view;
     }
 
-    private void question_Api()
-    {
+    private void question_Api() {
         progressBar_question.setVisibility(View.VISIBLE);
 
         bean b = (bean) getContext().getApplicationContext();
@@ -155,45 +144,41 @@ public class ViewTicket extends Fragment
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        ApiInterface cr=retrofit.create(ApiInterface.class);
+        ApiInterface cr = retrofit.create(ApiInterface.class);
 
         challengeRequestBean body = new challengeRequestBean();
 
-        com.tbx.gc.greatcash.challengeRequestPOJO.Data data=new com.tbx.gc.greatcash.challengeRequestPOJO.Data();
+        com.tbx.gc.greatcash.challengeRequestPOJO.Data data = new com.tbx.gc.greatcash.challengeRequestPOJO.Data();
 
         body.setAction("ticket_question");
 
         data.setUserId(id);
 
-        Log.e("iidd" , pref.getString("id" , ""));
+        Log.e("iidd", pref.getString("id", ""));
 
         body.setData(data);
 
         Call<QuestionBean> call = cr.questionApi(body);
 
 
-        call.enqueue(new Callback<QuestionBean>()
-        {
+        call.enqueue(new Callback<QuestionBean>() {
 
             @Override
-            public void onResponse(Call<QuestionBean> call, Response<QuestionBean> response)
-            {
-                Log.e("222","222");
+            public void onResponse(Call<QuestionBean> call, Response<QuestionBean> response) {
+                Log.e("222", "222");
 
                 progressBar_question.setVisibility(View.GONE);
 
-                if (response.body().getStatus().equals("1"))
-                {
+                if (response.body().getStatus().equals("1")) {
                     adapter.setGridData(response.body().getData());
-                    Log.e("3333","3333");
+                    Log.e("3333", "3333");
                 }
 
                 //progress.setVisibility(View.GONE);
             }
 
             @Override
-            public void onFailure(Call<QuestionBean> call, Throwable t)
-            {
+            public void onFailure(Call<QuestionBean> call, Throwable t) {
                 progressBar_question.setVisibility(View.INVISIBLE);
             }
 
@@ -202,13 +187,12 @@ public class ViewTicket extends Fragment
 
     }
 
-    private void HitApi()
-    {
+    private void HitApi() {
 
         Log.e("1111", "1111");
         progressBar.setVisibility(View.VISIBLE);
 
-        bean b = (bean)getContext().getApplicationContext();
+        bean b = (bean) getContext().getApplicationContext();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(b.BASE_URL)
@@ -218,9 +202,9 @@ public class ViewTicket extends Fragment
 
         ApiInterface cr = retrofit.create(ApiInterface.class);
 
-        Ticketrequest body=new Ticketrequest();
+        Ticketrequest body = new Ticketrequest();
 
-        DataReqticket data=new DataReqticket();
+        DataReqticket data = new DataReqticket();
 
         body.setAction("create_ticket");
 
@@ -228,15 +212,14 @@ public class ViewTicket extends Fragment
         data.setQuestionID(que_id);
         data.setReport(str_typed);
 
-        Log.e("questioiddd_send",""+que_id);
-        Log.e("questitypeee_send",""+str_typed);
+        Log.e("questioiddd_send", "" + que_id);
+        Log.e("questitypeee_send", "" + str_typed);
         body.setData(data);
 
         Call<ticketBean> call = cr.createTicket(body);
 
 
-        call.enqueue(new Callback<ticketBean>()
-        {
+        call.enqueue(new Callback<ticketBean>() {
 
             @Override
             public void onResponse(Call<ticketBean> call, Response<ticketBean> response) {
@@ -244,94 +227,83 @@ public class ViewTicket extends Fragment
 
                 progressBar.setVisibility(View.GONE);
 
-                if (response.body().getStatus().equals("1"))
-                {
+                if (response.body().getStatus().equals("1")) {
                     Toast.makeText(getActivity(), "Successfully Ticket Created", Toast.LENGTH_SHORT).show();
                     Log.e("3333", "3333");
                     ed_typed.setText("");
-
+                    text_sel.setText("Select Question");
+                    que_id = "";
                 }
 
                 progressBar.setVisibility(View.GONE);
             }
 
             @Override
-            public void onFailure(Call<ticketBean> call, Throwable t)
-            {
-              progressBar.setVisibility(View.GONE);
+            public void onFailure(Call<ticketBean> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
             }
 
         });
 
     }
 
-    private class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.ViewHolder>
-    {
+    private class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.ViewHolder> {
         private Context context;
-        private List<DataQuestion>dataQuesList;
+        private List<DataQuestion> dataQuesList;
 
-        public QuestionAdapter(FragmentActivity activity, List<DataQuestion> dataQuestionList)
-        {
-            this.context=activity;
-            this.dataQuesList=dataQuestionList;
+        public QuestionAdapter(FragmentActivity activity, List<DataQuestion> dataQuestionList) {
+            this.context = activity;
+            this.dataQuesList = dataQuestionList;
 
         }
 
 
-        public void setGridData(List<DataQuestion> list)
-        {
-           this.dataQuesList=list;
-           notifyDataSetChanged();
+        public void setGridData(List<DataQuestion> list) {
+            this.dataQuesList = list;
+            notifyDataSetChanged();
         }
 
         @NonNull
         @Override
-        public QuestionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
-        {
-            View view = LayoutInflater.from(context).inflate(R.layout.question_popup , viewGroup , false);
+        public QuestionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(context).inflate(R.layout.question_popup, viewGroup, false);
             return new ViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull QuestionAdapter.ViewHolder viewHolder, int i)
-        {
-           final DataQuestion dataQuestion=dataQuesList.get(i);
-           viewHolder.text_question.setText(dataQuestion.getQuestion());
+        public void onBindViewHolder(@NonNull QuestionAdapter.ViewHolder viewHolder, int i) {
+            final DataQuestion dataQuestion = dataQuesList.get(i);
+            viewHolder.text_question.setText(dataQuestion.getQuestion());
 
-           viewHolder.relative.setOnClickListener(new View.OnClickListener()
-           {
-               @Override
-               public void onClick(View view)
-               {
+            viewHolder.relative.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-                   String str_que=dataQuestion.getQuestion();
-                   que_id=dataQuestion.getQuestionID();
+                    String str_que = dataQuestion.getQuestion();
+                    que_id = dataQuestion.getQuestionID();
 
-                   text_sel.setText(str_que);
-                   dialog.dismiss();
+                    text_sel.setText(str_que);
+                    dialog.dismiss();
 
 
-               }
-           });
+                }
+            });
 
         }
 
         @Override
-        public int getItemCount()
-        {
+        public int getItemCount() {
             return dataQuesList.size();
         }
 
-        public class ViewHolder extends RecyclerView.ViewHolder
-        {
+        public class ViewHolder extends RecyclerView.ViewHolder {
             private TextView text_question;
             private RelativeLayout relative;
 
-            public ViewHolder(@NonNull View itemView)
-            {
+            public ViewHolder(@NonNull View itemView) {
                 super(itemView);
-                text_question=itemView.findViewById(R.id.text_country_name);
-                relative=itemView.findViewById(R.id.rel);
+                text_question = itemView.findViewById(R.id.text_country_name);
+                relative = itemView.findViewById(R.id.rel);
             }
         }
     }
